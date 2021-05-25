cbc2870d271f65438d8aac531f591167ba4757b6
Paul Johnecheck <pauljohnecheck@gmail.com>
2021-05-25 19:14:58 -0400

    gitignore the errors.txt, should be built when database is to be accurate across implementations

--- sMDT/cleanup.py
+++ sMDT/cleanup.py
@@ -1,3 +1,19 @@
+###############################################################################
+#   File: cleanup.py
+#   Author(s): Paul Johnecheck
+#   Date Created: 02 May, 2021
+#
+#   Purpose: This simple script just wipes the new_data and the locks directory.
+#   Crashed or prematurely ended programs can leave data here that can mess with things.
+#   This should only be ran while developing, this can and will cause data to not get put into the database.
+#   DO NOT RUN THIS FILE IN THE REAL LAB IF YOU KNOW WHAT YOU'RE DOING!!!!!
+#
+#   Known Issues:
+#
+#   Workarounds:
+#
+###############################################################################
+
 import db
 
 if __name__ == "__main__":
