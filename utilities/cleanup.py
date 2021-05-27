55173a008b8a6d3b24c5545e673f110d248be526
Paul Johnecheck <pauljohnecheck@gmail.com>
2021-05-26 20:08:39 -0400

    Fixed and moved the cleanup script, first of many in the utilitities file

--- utilities/cleanup.py
+++ utilities/cleanup.py
@@ -14,8 +14,8 @@
 #
 ###############################################################################
 
-from .db import db_manager
+from sMDT import db
 
 if __name__ == "__main__":
-    db_man = db_manager()
+    db_man = db.db_manager()
     db_man.cleanup()
