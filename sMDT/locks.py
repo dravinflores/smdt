6f7b69ce40bf471fb982b680a7f86a0c9d4c211d
Paul Johnecheck <pauljohnecheck@gmail.com>
2021-05-27 13:40:25 -0400

    locks are more pythonic, and only unlock the lock when it's destructed if it was the one that locked it in the first place.

--- sMDT/locks.py
+++ sMDT/locks.py
@@ -14,46 +14,38 @@
 #
 ###############################################################################
 
-# Import Preparation block.
-# Currently only needed so the tests in the mains work with the current imports.
 import os
-import sys
-
-# Gets the path of the current file being executed.
-sMDT_DIR = os.path.dirname(os.path.abspath(__file__))
-LOCK_DIR = os.path.join(sMDT_DIR, "locks")
-
-# Adds the folder that file is in to the system path
-sys.path.append(sMDT_DIR)
-
 import time
 
 
 class Lock:
-    
+    sMDT_DIR = os.path.dirname(os.path.abspath(__file__))
+    LOCK_DIR = os.path.join(sMDT_DIR, "locks")
 
     def __init__(self, key=""):
         '''
         Constructor, gets the locks key. Builds the lock's path out of the key
         '''
         self.key = key 
-        
-        self.lock_path = os.path.join(LOCK_DIR, key + ".lock")
+        self.locked_by_this = False
+        self.lock_path = os.path.join(self.LOCK_DIR, key + ".lock")
 
     def lock(self):
         '''
         This Lock becomes locked. A file key.lock is written to the lock path
         '''
-        if not os.path.isdir(LOCK_DIR):
-            os.mkdir(LOCK_DIR)
+        if not os.path.isdir(self.LOCK_DIR):
+            os.mkdir(self.LOCK_DIR)
         lock = open(self.lock_path, 'a')
         lock.write(self.key + " locked.")
+        self.locked_by_this = True
         lock.close()
 
     def unlock(self):
         '''
         This Lock becomes unlocked. The file key.lock is deleted from the lock path
         '''
+        self.locked_by_this = False
         if os.path.exists(self.lock_path):
             os.remove(self.lock_path)
 
@@ -71,11 +63,12 @@ class Lock:
             time.sleep(0.5)
 
     def __del__(self):
-        self.unlock()
+        if self.locked_by_this:
+            self.unlock()
 
-    @staticmethod
-    def cleanup():
-        if not os.path.isdir(LOCK_DIR):
-            os.mkdir(LOCK_DIR)
-        for filename in os.listdir(LOCK_DIR): 
-            os.remove(os.path.join(LOCK_DIR, filename))
+    @classmethod
+    def cleanup(Lock):
+        if not os.path.isdir(Lock.LOCK_DIR):
+            os.mkdir(Lock.LOCK_DIR)
+        for filename in os.listdir(Lock.LOCK_DIR):
+            os.remove(os.path.join(Lock.LOCK_DIR, filename))
