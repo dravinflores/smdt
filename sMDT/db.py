08e545eb3e3508dfd1b7d6ef2200b55d4b277080
schwier1 <85318693+schwier1@users.noreply.github.com>
2022-06-25 20:47:39 -0400

    Add UMich information

--- sMDT/db.py
+++ sMDT/db.py
@@ -11,6 +11,8 @@
 #   Workarounds: In order to better ensure that the database access is 
 #       restricted, the portalocker library is used; this is a cross-platform
 #       locking library.
+#  Modifications:
+#  2022-06, Sara Sawford, Reinhard Schwienhorst, add UMich information
 #
 ###############################################################################
 
@@ -21,6 +23,7 @@ import datetime
 import random
 import os
 import sys
+#import dbm # used only to make sure the database is in dmb.dumb format for compatibility
 
 import portalocker
 
@@ -38,10 +41,15 @@ class db:
         # Here are all the directories that are relevant to the database.
         # We are essentially asking for the directory that is two directories
         # up. All paths are then relative to this path.
+
+        #Gets first two directories of the full path involved in finding the dropbox
         self.dropbox_directory = Path(__file__).resolve().parents[1]
+        #uses above path to put the database in this directory as datdabase.s
         self.db_file = self.dropbox_directory / 'database.s'
         self.lock_file = self.dropbox_directory /'sMDT'/'locks'/'db_lock.lock'
+        #creates new directory from data in 'new data'
         self.new_data_dir = self.dropbox_directory / 'sMDT' / 'new_data'
+        #print("Database directory ",self.new_data_dir)
 
         # We need to check if the database lock file exists. If
         # not, then we'll have to make those directories.
@@ -63,11 +71,23 @@ class db:
         # Before anything, we will go ahead and convert the path object into a 
         # normal string. This is because portalocker has it's own internal 
         # method of opening the database.
+
+        #turning path into string
         s = str(self.lock_file.resolve())
+
+        #try/except lines
+        #try lets you test a block for errors
+        #if there are errors, use except to handle them. if not, runs all good
         try:
+            #r+ probably means to read and write to the file
             with portalocker.Lock(s, 'r+', timeout=30) as locked_file:
                 # We've got exclusive access to the database.
+
+                #remembering self.db_file is the database.s
                 db_file = str(self.db_file.resolve())
+                #print("Database file format: ",dbm.whichdb(db_file))
+
+                #'r' is just a read only file
                 return_dict = shelve.open(db_file, 'r')
         except portalocker.LockException:
             # Just in-case we can't open the database, we'll return an
@@ -81,12 +101,17 @@ class db:
         try:
             with portalocker.Lock(s, 'r+', timeout=30) as locked_file:
                 shelve_obj.close()
+
+        #if the database doesn't exist, there is nothing to close
         except portalocker.LockException as e:
             pass
 
     def size(self):
+        #since open_shelve returns the dictionary (either blank or database.s), tube_dict = return_dict
         tube_dict = self.open_shelve()
         number_of_tubes = len(tube_dict)
+
+        #close_shelve is an action, not return value
         self.close_shelve(tube_dict)
         return number_of_tubes
 
@@ -95,22 +120,32 @@ class db:
         timestamp = dt.timestamp()
 
         filename = str(timestamp) + str(random.randrange(0, 999)) + ".tube"
+        #creates a new .tube file named after the data,time and random number. located in new_data
         file_obj = self.new_data_dir / filename
-
         if not file_obj.exists():
+            #parents=True creates any directories that don't exist yet
+            #exist_ok=True does nothing if a directory already exists
             file_obj.parent.mkdir(parents=True, exist_ok=True)
+
+            #creates file.txt at file_obj path
             file_obj.touch()
 
         s = str(self.lock_file.resolve())
         try:
             with portalocker.Lock(s, 'r+', timeout=30) as locked_file:
+
+                #wb+ to write a binary file
                 with file_obj.open('wb+') as f:
+                    #writing tube to file_obj we just opened
+                    #print("writing tube ",tube.get_ID()," to file ",filename)
                     pickle.dump(tube, f)
         except portalocker.LockException as e:
+            #print("Error trying to write to database file ",filename)
             pass
 
     def get_tube(self, barcode):
         tubes = self.open_shelve()
+        #if the tube with the specific parameter barcode exists, then return it and close the shelf
         try:
             ret_tube = tubes[barcode]
         except KeyError:
@@ -122,6 +157,10 @@ class db:
 
     def get_tubes(self, selection=None):
         tubes = self.open_shelve()
+
+        #if selection=None, then goes to else statement
+        #if selection != None, appends the tubes in the list to return list
+        #else, returns the initial tube values
         if selection:
             ret_tubes = []
             for ID in selection:
@@ -135,12 +174,14 @@ class db:
         return ret_tubes
 
     def get_IDs(self):
+        #returns keys of dictionary
         tubes = self.open_shelve()
         ret_ids = list(tubes.keys())
         self.close_shelve(tubes)
         return ret_ids
 
     def delete_tube(self, tube_id):
+        #unless the remove is in umich_tube(), only creates .del.tube, doesn't remove from database
         if type(tube_id) is not str:
             tube_id = tube_id.get_ID()
 
@@ -165,6 +206,7 @@ class db:
             pass
 
     def overwrite_tube(self, tube):
+        #doesn't seem to remove the wrong tube, just edits one of the ids
         dt = datetime.datetime.now()
         timestamp = dt.timestamp()
 
@@ -187,6 +229,7 @@ class db_manager:
     def __init__(self, db_path=None, archive=True, testing=False):
         self.dropbox_directory = Path(__file__).resolve().parents[1]
 
+        #if db_path = None, create a new database
         if not db_path:
             self.db_file = self.dropbox_directory / 'database.s'
 
@@ -205,7 +248,7 @@ class db_manager:
             s = str(self.lock_file.resolve())
             try:
                 with portalocker.Lock(s, 'r+', timeout=30) as locked_file:
-                    tubes = shelve.open(self.path, flag='n')
+                    tubes = shelve.open(self.path, 'n')
             except portalocker.LockException as e:
                 pass
         else:
@@ -228,6 +271,7 @@ class db_manager:
             pickler.pickle_darkcurrent()
             pickler.pickle_bentness()
             pickler.write_errors()
+            #pickler.pickle_umich()
 
         # Now we lock the database to write.
         s = str(self.lock_file.resolve())
@@ -238,13 +282,12 @@ class db_manager:
 
                 # Check if the stored database is more recent.
                 if not (len(tubes) or len(tubes) < 50) and not self.testing:
-                    print_str = f"\nThe Database has been corrupted. " 
-                    print_str += "Size is {}.\n".format(len(tubes))
+                    print("\nThe Database has been corrupted. ") 
+                    print_str = "Size is {}.\n".format(len(tubes))
                     print_str += "The database will continue to sync. \n"
-                    t = datetime.datetime.now()
-                    time_str = f'At Time: {t.strftime("%d-%b-%Y %H:%M:%S")}\n\n'
-                    print_str += time_str
                     print(print_str)
+                    t = datetime.datetime.now()
+                    print("At Time:",t.strftime("%d-%b-%Y %H:%M:%S"),"\n\n")
 
                     error_dir = Path('DatabaseError')
                     ext = '.txt'
@@ -259,13 +302,19 @@ class db_manager:
 
                 addcount = editcount = delcount = 0
                 t = time.localtime()
+                #
+                # loop over files in new_data directory
                 for filename in os.listdir(self.new_data_dir):
                     new_data_file = open(
                         os.path.join(self.new_data_dir, filename), 'rb'
                     ) 
 
-                    tube = pickle.load(new_data_file) 
-                    new_data_file.close()
+                    try:
+                        tube = pickle.load(new_data_file) 
+                        new_data_file.close()
+                    except EOFError:
+                        # this file is being written to as we're trying to open it, skip for now
+                        continue
 
                     if filename.endswith(".del.tube"):
                         if tube.get_ID() in tubes:
@@ -328,8 +377,10 @@ class db_manager:
                                 "into database."
                             )
 
+
                         if tube.get_ID() in tubes: 
                             # add the tubes to the database
+                            #print("Tube ",tube.get_ID()," already exists")
                             temp = tubes[tube.get_ID()] + tube
                             tubes[tube.get_ID()] = temp
                         else:
